fef4138899fb3611679314422e9ca0ea1f942530
FrankLee <FrankLee@Arrrwuwu.local>
2022-08-03 17:15:02 +0800

    UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,123 @@
-import logo from './logo.svg';
-import './App.css';
+// import "./App.css";
+import "./style.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <div class="calculator dark">
+        <div class="display-screen">
+          <div id="display"></div>
+        </div>
+        <div class="buttons">
+          <table>
+            <tr>
+              <td>
+                <button class="buttonClear" id="ac">
+                  AC
+                </button>
+              </td>
+              <td>
+                <button class="btn-operator" id="percent">
+                  %
+                </button>
+              </td>
+              <td>
+                <button class="btn-operator" id="minus">
+                  +/-
+                </button>
+              </td>
+              <td>
+                <button class="btn-operator" id="plus">
+                  +
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <button class="btn-number" id="7">
+                  7
+                </button>
+              </td>
+              <td>
+                <button class="btn-number" id="8">
+                  8
+                </button>
+              </td>
+              <td>
+                <button class="btn-number" id="9">
+                  9
+                </button>
+              </td>
+              <td>
+                <button class="btn-operator" id="minus">
+                  -
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <button class="btn-number" id="4">
+                  4
+                </button>
+              </td>
+              <td>
+                <button class="btn-number" id="5">
+                  5
+                </button>
+              </td>
+              <td>
+                <button class="btn-number" id="6">
+                  6
+                </button>
+              </td>
+              <td>
+                <button class="btn-operator" id="mul">
+                  x
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <button class="btn-number" id="1">
+                  1
+                </button>
+              </td>
+              <td>
+                <button class="btn-number" id="2">
+                  2
+                </button>
+              </td>
+              <td>
+                <button class="btn-number" id="3">
+                  3
+                </button>
+              </td>
+              <td>
+                <button class="btn-operator" id="divided">
+                  ÷
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td colspan="2">
+                <button class="button0" id="0">
+                  0
+                </button>
+              </td>
+              <td>
+                <button class="btn-operator" id="doc">
+                  .
+                </button>
+              </td>
+              <td>
+                <button class="btn-operator" id="equal">
+                  =
+                </button>
+              </td>
+            </tr>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
